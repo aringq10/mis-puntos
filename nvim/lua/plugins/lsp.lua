819be279449d2df7cebdbd12e76167ed350e9878
Aringas Civilka <aringas15@gmail.com>
2026-08-09 14:19:03 +0300

    cut down on lsp keybinds

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -22,55 +22,25 @@ return {
       'saghen/blink.cmp',
     },
     config = function()
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
-        callback = function(event)
-          local map = function(keys, func, desc, mode)
-            mode = mode or 'n'
-            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local builtin = require("telescope.builtin")
+          local map = function(keys, fn, desc)
+            vim.keymap.set("n", keys, fn, { buffer = args.buf, desc = "LSP: " .. desc })
           end
 
-          map('K', function() vim.lsp.buf.hover({ border = "rounded" }) end, 'Hover')
-          map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
-          map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
-          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-          map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
-          map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
-          map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
-
-          -- Highlight thing under cursor
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client:supports_method('textDocument/documentHighlight', event.buf) then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.document_highlight,
-            })
-
-            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-              buffer = event.buf,
-              group = highlight_augroup,
-              callback = vim.lsp.buf.clear_references,
-            })
-
-            vim.api.nvim_create_autocmd('LspDetach', {
-              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-              callback = function(event2)
-                vim.lsp.buf.clear_references()
-                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-              end,
-            })
-          end
-
-          if client and client:supports_method('textDocument/inlayHint', event.buf) then
-            map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-            end, '[T]oggle Inlay [H]ints')
-          end
+          map("grd", builtin.lsp_definitions,               "Definition")
+          map("grr", builtin.lsp_references,                "References")
+          map("gri", builtin.lsp_implementations,           "Implementation")
+          map("grt", builtin.lsp_type_definitions,          "Type Definition")
+          map("grs", builtin.lsp_document_symbols,          "Document Symbols")
+          map("grw", builtin.lsp_dynamic_workspace_symbols, "Workspace Symbols")
+          map("grci", builtin.lsp_incoming_calls,           "Incoming Calls")
+          map("grco", builtin.lsp_outgoing_calls,           "Outgoing Calls")
+          map("grD", vim.lsp.buf.declaration,               "Declaration")
+          map("grn", vim.lsp.buf.rename,                    "Rename")
+          map("gra", vim.lsp.buf.code_action,               "Code Action")
+          map("K",   function() vim.lsp.buf.hover({ border = "rounded" }) end, "Hover Docs")
         end,
       })
 
